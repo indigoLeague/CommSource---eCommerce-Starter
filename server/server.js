9e0d3e146b264a58119020138cc7ef66c4b6ccf5
Chris Flannery <chriswillsflannery@gmail.com>
2019-11-25 12:19:48 -0500

    added webpack hot reloading with webpack-dev-server

--- server/server.js
+++ server/server.js
@@ -9,14 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-const server = http.createServer(
-  // {
-  //   key: fs.readFileSync(path.resolve(__dirname, './ssl_diane/server.key')),
-  //   // // is required to allow localhost to run on HTTPS
-  //   // // otherwise you either get an error or a certified invalid warning
-  //   cert: fs.readFileSync(path.resolve(__dirname, './ssl_diane/server.crt')),
-  // },
-  app
-);
 
-server.listen(3000, () => console.log('listening on 3000'));
+
+app.listen(3000, () => console.log('listening on 3000'));
