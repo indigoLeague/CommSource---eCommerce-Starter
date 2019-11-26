2b109b8c2b2f791dcd1d69ec8da355b040e92f7b
Chris Flannery <chriswillsflannery@gmail.com>
2019-11-26 17:04:33 -0500

    tried implementing route

--- server/controllers/item-controller.js
+++ server/controllers/item-controller.js
@@ -1,10 +1,8 @@
-
-const fs = require('fs');
-const path = require('path');
 const Item = require('../../database/models/item-model');
 
 const itemController = {};
 
+// we need to await loading all items from db before moving back to item-router
 itemController.getAllItems = async (req, res, next) => {
   // loads an array of items as objects
   await Item.find((err, suc) => {
@@ -17,11 +15,41 @@ itemController.getAllItems = async (req, res, next) => {
   return next();
 };
 
-itemController.getItem = (req, res, next) => {
-  console.log('getting item in item controller');
-  const items = Item.find((err, suc) => {
-    console.log('suc', suc);
-  });
+itemController.buyItems = (req, res, next) => {
+  console.log('in buyItems, item-controller');
+  const toBeUpdated = Object.entries(req.body);
+
+  // items to updated (as array)
+  console.log('tobeupdated', toBeUpdated);
+
+  // all items in db (as array)
+  // console.log('reslocals', res.locals.allItems);
+
+  for (let i = 0; i < toBeUpdated.length; i++) {
+    const currentTBU = toBeUpdated[i];
+
+    // query database for particular item that matches currentTBU name
+
+    Item.find({ name: currentTBU[0] }, (err, succ) => {
+      const oldQuantity = succ[0].quantity;
+      const newQuantity = oldQuantity - currentTBU[1].quantity;
+      // update that particular item's quantity by subtracting
+      Item.update({ name: succ[0].name }, { quantity: newQuantity }, (er, suc) => {
+        if (err) {
+          console.log('error:', err.stack);
+        } else {
+          console.log('succ', succ);
+        }
+      });
+    });
+  }
 };
+
+// itemController.getItem = (req, res, next) => {
+//   console.log('getting item in item controller');
+//   const items = Item.find((err, suc) => {
+//     console.log('suc', suc);
+//   });
+// };
 
 module.exports = itemController;
--- server/routes/item-router.js
+++ server/routes/item-router.js
@@ -4,13 +4,17 @@ const router = express.Router();
 
 const itemController = require('../controllers/item-controller');
 
+// route to load all items from db
 router.get('/loaditems', itemController.getAllItems, (req, res, next) => {
-  console.log('load all items in item router');
   res.status(200).send(res.locals.allItems);
 });
 
-router.get('/:id', itemController.getItem, (req, res, next) => {
-  console.log('retrieved item in item router');
+router.post('/buyitems', itemController.getAllItems, itemController.buyItems, (req, res, next) => {
+  console.log('in router post: buy items, item-router');
 });
+
+// router.get('/:id', itemController.getItem, (req, res, next) => {
+//   console.log('retrieved item in item router');
+// });
 
 module.exports = router;
